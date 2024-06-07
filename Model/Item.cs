ae835b41c48a9968c1ed4a480f4cad9d7879a1e2
thayuyu <yunes1703@icloud.com>
2024-06-07 14:41:06 +0200

    Fixed a problem while uploading the prject

---
+++ Model/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace ServerShopDisplay.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PictureLink { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public Item() { }
+    }
+}
